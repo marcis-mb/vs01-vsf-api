e733a860ab01c5530d82236a4a6e08b2d8af19fa
Marcis Pumpurs <marcis.pumpurs@magebit.com>
2021-07-22 14:43:49 +0300

    Added copyright info

--- src/api/extensions/weather-widget/index.js
+++ src/api/extensions/weather-widget/index.js
@@ -1,3 +1,17 @@
+/**
+ * This file is part of the Magebit WeatherWidget package.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magebit WeatherWidget
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2021 Magebit, Ltd. (https://magebit.com/)
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 import { Router } from 'express';
 import request from 'request';
 import cache from '../../../lib/cache-instance'
